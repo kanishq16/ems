b84dcdae139699b6f34a2933c9e2a352b1c09d9b
Kanishq <kanishqanand91@gmail.com>
2026-09-09 23:06:41 +0530

    fixed login.jsx file and added state for email and password and also added onChange event for both input fields.

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 
 const Login = () => {
+
+  const submitHandler = (e) => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    e.preventDefault() 
+    console.log("email is", email)
+    console.log("password is", password)
+
+    setEmail("") 
+    setPassword("")
+  }
+
   return (
     <div className = 'flex h-screen w-screen items-center justify-center'>
-        <div className='border-2 border-red-600'>
-            <form className='flex flex-col items-center justify-center'>
-                <input type="eamil" placeholder='Enter your email' />
-                <input type="password" placeholder='Enter Password' />
+        <div className='border-2 rounded-xl border-emerald-600 p-20'>
+            <form onSubmit={(e) => {
+              submitHandler(e)
+            }} className='flex flex-col items-center justify-center'>
+                <input 
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value) 
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400'type="email" placeholder='Enter your email' />
+                <input 
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value) 
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full mt-3 placeholder:text-gray-400'type="password" placeholder='Enter Password' />
+                <button className='mt-7 text-white border-none outline-none bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white'>Log in</button>
             </form>
         </div> 
     </div>
