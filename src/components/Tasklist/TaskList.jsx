3ce40d7d88f82db383c4c020ea1244c8f320b935
Kanishq <kanishqanand91@gmail.com>
2026-09-15 18:02:08 +0530

    updated code and fixed some errors !

--- src/components/Tasklist/TaskList.jsx
+++ src/components/Tasklist/TaskList.jsx
@@ -1,56 +1,29 @@
 import React from 'react' 
+import NewTask from './Newtask'
+import CompleteTask from './Completetask'
+import AcceptTask from './AcceptTask'
+import FailedTask from './FailedTask'
 
 const TaskList = ({data}) => {
     return (
-        <div id='tasklist'className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
-            <div className='flex-shrink-0h-full w-[300px] p-5 bg-red-400 rounded-xl'>
-                  <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>
-                        High
-                    </h3>
-                    <h4 className='text-sm'>20 feb 2024</h4>
-                  </div>
-                  <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube video</h2>
-                  <p className='text-sm mt-2'>
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-                  </p>
-            </div>
-              <div className='flex-shrink-0h-full w-[300px] p-5 bg-blue-400 rounded-xl'>
-                  <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>
-                        High
-                    </h3>
-                    <h4 className='text-sm'>20 feb 2024</h4>
-                  </div>
-                  <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube video</h2>
-                  <p className='text-sm mt-2'>
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-                  </p>
-            </div>
-              <div className='flex-shrink-0h-full w-[300px] p-5 bg-yellow-400 rounded-xl'>
-                  <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>
-                        High
-                    </h3>
-                    <h4 className='text-sm'>20 feb 2024</h4>
-                  </div>
-                  <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube video</h2>
-                  <p className='text-sm mt-2'>
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-                  </p>
-            </div>
-              <div className='flex-shrink-0h-full w-[300px] p-5 bg-green-400 rounded-xl'>
-                  <div className='flex justify-between items-center'>
-                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>
-                        High
-                    </h3>
-                    <h4 className='text-sm'>20 feb 2024</h4>
-                  </div>
-                  <h2 className='mt-5 text-2xl font-semibold'>Make a Youtube video</h2>
-                  <p className='text-sm mt-2'>
-                    lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-                  </p>
-            </div>
+        <div id='tasklist'className='h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
+            {data.tasks.map((elem, idx) => {
+              
+              if(elem.active){
+
+                return <AcceptTask  />
+              }
+              if(elem.newTask) {
+                return <NewTask />
+              }
+              if(elem.completed) {
+                return <CompleteTask />
+              }
+              if(elem.failed) {
+                return <FailedTask/>
+              }
+
+            })}
         </div>
     )
 }
--- src/others/Header.jsx
+++ src/others/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 
-const Header = ({data}) => {
+const Header = () => {
+
+    // const [username, setUsername] = useState('')
+
+    // if(!data){
+    //     setUsername('Admin')
+    // } else{
+    //     setUsername(data.firstName)
+    // }
+
+    const logOutUser = () => {
+
+    }
 
     
     return (
         <div className='flex items-end justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>{data.firstName} 👋</span></h1>
-            <button className='bg-red-600 text-lg font-medium px-5 py-2 rounded-sm'>Log Out</button>
+            <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'>username👋</span></h1>
+            <button onCLick={logOutUser} className='bg-red-600 text-lg font-medium px-5 py-2 rounded-sm'>Log Out</button>
         </div>
     )
 }
